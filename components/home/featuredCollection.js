1ca8bf776b04a61c4618da073e0761a15d3c3f2e
ibrahim <ibrahim.mhd.ibrahim@gmail.com>
2021-11-06 23:16:11 +0200

    added featured collection

---
+++ components/home/featuredCollection.js
@@ -0,0 +1,23 @@
+import classes from "/assets/styles/partials/featuredCollection.module.scss";
+
+const FeaturedCollection = () => {
+    return (
+        <div className={classes.collectionContainer}>
+            <div className={classes.collectionColumn}>
+                <img src="https://via.placeholder.com/500x600"/>
+            </div>
+            <div className={classes.collectionColumn}>
+                <div className={classes.content}>
+                    <h3>WINTER IS HERE</h3>
+                    <h2>NEW COLLECTION</h2>
+                    <a className="btn" href="#">SHOP NOW</a>
+                </div>
+            </div>
+            <div className={classes.collectionColumn}>
+                <img src="https://via.placeholder.com/500x600"/>
+            </div>
+        </div>
+    );
+}
+
+export default FeaturedCollection;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import classes from "/assets/styles/pages/homePage.module.scss";
 import MainSlider from "../components/home/mainSlider";
 import CategoryList from "../components/home/categoryList";
+import FeaturedCollection from "../components/home/featuredCollection";
 
 function Home() {
     return (
         <div className={classes.homepage}>
             <MainSlider />
             <CategoryList />
+            <FeaturedCollection />
         </div>
     );
 }
