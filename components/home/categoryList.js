9461c75004fcdae3711eb087b9556f9a90c539c4
ibrahim <ibrahim.mhd.ibrahim@gmail.com>
2021-11-07 14:44:23 +0200

    rearranged styling

--- components/home/categoryList.js
+++ components/home/categoryList.js
@@ -1,27 +1,28 @@
-import classes from "/assets/styles/partials/categoryList.module.scss"
+import classes from "/assets/styles/partials/categoryList.module.scss";
+import grid from "/assets/styles/layouts/grid/flexGrid.module.scss";
 
 const CategoryList = () => {
     return (
-        <div className={classes.categoryContainer + " container"}>
-            <div className={classes.categoryColumn}>
+        <div className={`${grid.containerFlex} ${classes.categoryContainer} container`}>
+            <div className={`${grid.columnFlex} ${classes.categoryColumn}`}>
                 <div className={classes.categoryBlock}>
                     <h3>Category Title</h3>
                     <img src="https://via.placeholder.com/400x400"/>
                 </div>
             </div>
-            <div className={classes.categoryColumn}>
+            <div className={`${grid.columnFlex} ${classes.categoryColumn}`}>
                 <div className={classes.categoryBlock}>
                     <h3>Category Title</h3>
                     <img src="https://via.placeholder.com/400x400"/>
                 </div>
             </div>
-            <div className={classes.categoryColumn}>
+            <div className={`${grid.columnFlex} ${classes.categoryColumn}`}>
                 <div className={classes.categoryBlock}>
                     <h3>Category Title</h3>
                     <img src="https://via.placeholder.com/400x400"/>
                 </div>
             </div>
-            <div className={classes.categoryColumn}>
+            <div className={`${grid.columnFlex} ${classes.categoryColumn}`}>
                 <div className={classes.categoryBlock}>
                     <h3>Category Title</h3>
                     <img src="https://via.placeholder.com/400x400"/>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import "../assets/styles/globals.scss";
-import "../assets/styles/modules/grid.scss";
 import Header from "../components/layouts/header";
 import Footer from "../components/layouts/footer";
 
