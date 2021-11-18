5b0822640e887c84d29e1394a1322fec39d0e600
ibrahim <ibrahim.mhd.ibrahim@gmail.com>
2021-11-18 21:40:18 +0200

    added server side props instead of useEffect

--- components/home/featuredProducts.js
+++ components/home/featuredProducts.js
@@ -1,27 +1,12 @@
 import grid from "/assets/styles/layouts/grid/flexGrid.module.scss";
 import classes from "/assets/styles/partials/featuredProducts.module.scss";
 import Product from "../partials/product";
-import {useEffect, useState} from "react";
 
-const FeaturedProducts = () => {
-    const [featuredProducts, setFeaturedProducts] = useState([]);
-
-    useEffect(async () => {
-        const response = await fetch("/api/products", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        const productsData = await response.json();
-        setFeaturedProducts(productsData);
-    }, [setFeaturedProducts]);
-
-
+const FeaturedProducts = (props) => {
     const productsList = () => {
-        return featuredProducts.map((product) => (
+        return props.products.map((product) => (
             <div key={product.id} className={grid.columnFlex}>
-                <Product title={product.name} smallDescription={product.smallDescription} price={product.price} image={product.image} />
+                <Product id={product.id} title={product.name} smallDescription={product.smallDescription} price={product.price} image={product.image} />
             </div>
         ));
     }
@@ -35,5 +20,4 @@ const FeaturedProducts = () => {
         </section>
     );
 }
-
 export default FeaturedProducts;
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,31 @@ import CategoryList from "../components/home/categoryList";
 import FeaturedCollection from "../components/home/featuredCollection";
 import FeaturedProducts from "../components/home/featuredProducts";
 
-function Home() {
+function Home(props) {
     return (
         <div className={classes.homepage}>
             <MainSlider />
             <CategoryList />
             <FeaturedCollection />
-            <FeaturedProducts />
+            <FeaturedProducts products={props.products} />
         </div>
     );
 }
 
 export default Home;
+
+export const getServerSideProps = async () => {
+    const response = await fetch("http://localhost:3000/api/products", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const products = await response.json();
+
+    return {
+        props: {
+            products
+        }
+    };
+};
